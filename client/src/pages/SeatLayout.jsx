6299356f4b532c1e8f7048367ceb4ecc4702dc27
SOUVIK SAHA <souviksaha@crldomain.com>
2025-08-01 17:27:43 +0530

    Seat Layout Variables Set

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
+import Loading from '../components/Loading'
 
 const SeatLayout = () => {
-  return (
-    <div>SeatLayout</div>
+
+  const navigate = useNavigate()
+
+  const {id, date} = useParams()
+  const [selectedSeats, setSelectedSeats] = useState([])
+  const [selectedTime, setSelectedTime] = useState(null)
+  const [show, setShow] = useState(null)
+
+  const getShow = async() => {
+    const show = dummyShowsData.find(show => show._id === id)
+    if(show) {
+      setShow({
+        movie: show,
+        dateTime: dummyDateTimeData
+      })
+    }
+  }
+
+  useEffect(() => {
+    getShow()
+  }, [])
+
+  return show? (
+    <div>
+      
+    </div>
+  ) : (
+    <Loading />
   )
 }
 
