dec6f1926b50196ce081acdc96978a44127e5031
SOUVIK SAHA <souviksaha@crldomain.com>
2025-08-05 15:30:05 +0530

    AdminLayout done

---
+++ client/src/pages/admin/AdminLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
+import AdminNavbar from '../../components/admin/AdminNavbar'
+import { Outlet } from 'react-router-dom'
+import AdminSidebar from '../../components/admin/AdminSidebar'
+
+const AdminLayout = () => {
+  return (
+    <>
+      <Toaster />
+      <AdminNavbar />
+      <div className='flex'>
+        <AdminSidebar />
+        <div className='flex-1 px-4 py-10 md:px-10 h-[calc(100vh-64px)] overflow-y-auto'>
+          <Outlet />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default AdminLayout
