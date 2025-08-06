345efb166a41e35dbae788c7e1e519896aa53e22
SOUVIK SAHA <souviksaha@crldomain.com>
2025-08-06 13:43:05 +0530

    ListBookings page done

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Loading from '../../components/Loading'
+import { dummyBookingData } from '../../assets/assets'
+import AdminTitle from '../../components/admin/AdminTitle'
+import { dateFormat } from '../../lib/dateFormat'
 
 const ListBookings = () => {
-  return (
-    <div>ListBookings</div>
-  )
+
+    const currency = import.meta.env.VITE_CURRENCY
+    const [bookings, setBookings] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    const fetchAllBookings = async () => {
+        setBookings(dummyBookingData)
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        fetchAllBookings()
+    }, [])
+
+    return !isLoading ? (
+        <>
+            <AdminTitle text1={`List`} text2={`Bookings`} />
+            <table className='w-full border-collapse rounded-md overflow-hidden text-nowrap'>
+                <thead>
+                    <tr className='bg-primary/20 text-left text-white'>
+                        <th className='p-2 font-medium pl-5'>User Name</th>
+                        <th className='p-2 font-medium'>Movie Name</th>
+                        <th className='p-2 font-medium'>Show Time</th>
+                        <th className='p-2 font-medium'>Seats</th>
+                        <th className='p-2 font-medium'>Amount</th>
+                    </tr>
+                </thead>
+
+                <tbody className='text-sm font-light'>
+                    {bookings.map((item, index) => (
+                        <tr key={index} className='border-b border-primary/20 bg-primary/5 even:bg-primary/10'>
+                            <td className='p-2 min-w-45 pl-5'>{item.user.name}</td>
+                            <td className='p-2'>{item.show.movie.title}</td>
+                            <td className='p-2'>{dateFormat(item.show.showDateTime)}</td>
+                            <td className='p-2'>{Object.keys(item.bookedSeats).map(seat => item.bookedSeats[seat]).join(", ")}</td>
+                            <td className='p-2'>{currency} {item.amount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    ) : <Loading />
 }
 
 export default ListBookings
