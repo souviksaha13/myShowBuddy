fdad497c012ca4da89e9f7a8f9198db0b6793094
SOUVIK SAHA <souviksaha@crldomain.com>
2025-08-11 17:50:10 +0530

    ListBookings dummy to original done

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -3,21 +3,34 @@ import Loading from '../../components/Loading'
 import { dummyBookingData } from '../../assets/assets'
 import AdminTitle from '../../components/admin/AdminTitle'
 import { dateFormat } from '../../lib/dateFormat'
+import { useAppContext } from '../../context/AppContext'
 
 const ListBookings = () => {
 
     const currency = import.meta.env.VITE_CURRENCY
+    const { axios, getToken, user } = useAppContext()
+
     const [bookings, setBookings] = useState([])
     const [isLoading, setIsLoading] = useState(true)
 
     const fetchAllBookings = async () => {
-        setBookings(dummyBookingData)
+        try {
+            const { data } = await axios.get('/api/admin/all-bookings', {
+                headers: { Authorization: `Bearer ${await getToken()}`}
+            })
+            setBookings(data.bookings)
+            console.log(data.bookings)
+        } catch (error) {
+            console.error(error)
+        }
         setIsLoading(false)
     }
 
     useEffect(() => {
-        fetchAllBookings()
-    }, [])
+        if(user) {
+            fetchAllBookings()
+        }
+    }, [user])
 
     return !isLoading ? (
         <>
