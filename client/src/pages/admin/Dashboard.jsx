807b1f740b4e7df55aada0824720e167ef208a97
SOUVIK SAHA <souviksaha@crldomain.com>
2025-08-11 17:11:28 +0530

    Dashboard dummy data to original data

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -5,10 +5,15 @@ import Loading from '../../components/Loading'
 import AdminTitle from '../../components/admin/AdminTitle'
 import BlurCircle from '../../components/BlurCircle'
 import { dateFormat } from '../../lib/dateFormat'
+import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast'
 
 const Dashboard = () => {
 
     const currency = import.meta.env.VITE_CURRENCY
+
+    const { axios, getToken, user, image_base_url } = useAppContext()
+
 
     const [dashboardData, setDashboardData] = useState({
         totalBookings: 0,
@@ -27,13 +32,30 @@ const Dashboard = () => {
     ]
 
     const fetchDashboardData = async () => {
-        setDashboardData(dummyDashboardData)
-        setLoading(false)
+        try {
+            console.log("function reached 1")
+            const tmdbToken = await getToken()
+            const { data } = await axios.get('/api/admin/dashboardData', {
+                headers: { Authorization: `Bearer ${tmdbToken}`}
+            })
+            console.log("function reached 1")
+            if(data.success) {
+                console.log(data.dashboardData)
+                setDashboardData(data.dashboardData)
+                setLoading(false)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(`Error fetching movies: `, error)
+        }
     }
 
     useEffect(() => {
-        fetchDashboardData()
-    }, [])
+        if(user) {
+            fetchDashboardData()
+        }
+    }, [user])
 
   return !loading ? (
     <>
@@ -60,7 +82,7 @@ const Dashboard = () => {
             <BlurCircle top='100px' left='-10%' />
             {dashboardData.activeShows.map((show) => (
                 <div key={show._id} className='w-55 cursor-pointer rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300'>
-                    <img src={show.movie.poster_path} alt="" className='h-60 w-full object-cover' />
+                    <img src={image_base_url + show.movie.poster_path} alt="" className='h-60 w-full object-cover' />
                     <p className='font-medium p-2 truncate'>{show.movie.title}</p>
 
                     <div className='flex items-center justify-between px-2'>
