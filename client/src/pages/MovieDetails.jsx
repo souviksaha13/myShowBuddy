48f05a09830834c61fa5f0a0b5cf2459d3c70d4a
SOUVIK SAHA <souviksaha@crldomain.com>
2025-08-11 18:12:45 +0530

    Movie Details page populated with original data and favourites functionality

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -7,19 +7,42 @@ import timeFormat from '../lib/timeFormat'
 import DateSelect from '../components/DateSelect'
 import MovieCard from '../components/MovieCard'
 import Loading from '../components/Loading'
+import { useAppContext } from '../context/AppContext'
+import toast from 'react-hot-toast'
 
 const MovieDetails = () => {
   const navigate = useNavigate()
   const {id} = useParams()
   const [show, setShow] = useState(null)
 
+  const { axios, shows, getToken, user, fetchFavMovies, favouriteMovies, image_base_url} = useAppContext()
+
   const getShow = async() => {
-    const show = dummyShowsData.find(show => show._id === id)
-    if(show) {
-      setShow({
-        movie: show,
-        dateTime: dummyDateTimeData
-      })
+    try {
+      const { data } = await axios.get(`/api/show/${id}`)
+
+      if(data.success) {
+        setShow(data)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const handleFavourite = async() => {
+    try {
+      if(!user) {
+        return toast.error("Please login to proceed")
+      }
+
+      const { data } = await axios.post('/api/user/update-favourite', {movieId: id}, {headers: { Authorization: `Bearer ${await getToken()}`}})
+
+      if(data.success) {
+        await fetchFavMovies()
+        toast.success(data.message)
+      }
+    } catch (error) {
+      console.error(error)
     }
   }
 
@@ -32,7 +55,7 @@ const MovieDetails = () => {
       {/* Movie Details Section */}
       <div className='flex flex-col md:flex-row gap-8 max-w-6xl mx-auto'>
         {/* Movie poster */}
-        <img src={show.movie.poster_path} alt='' className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
+        <img src={image_base_url + show.movie.poster_path} alt='' className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
 
         <div className='relative flex flex-col gap-3'>
           <BlurCircle top='100px' left='100px'/>
@@ -56,7 +79,7 @@ const MovieDetails = () => {
             </button>
             <a href='#dateSelect' className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95' >Buy Tickets</a>
             <button className='bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95'>
-              <Heart className={`w-5 h-5`} />
+              <Heart onClick={handleFavourite} className={`w-5 h-5 ${favouriteMovies.find((movie) => movie._id === id) ? 'fill-primary text-primary' : ''}`} />
             </button>
           </div>
         </div>
@@ -68,7 +91,7 @@ const MovieDetails = () => {
         <div className='flex items-center gap-4 px-4 w-max'>
           {show.movie.casts.slice(0,12).map((cast, index) => (
             <div key={index} className='flex flex-col items-center text-center' >
-              <img src={cast.profile_path} alt="" className='rounded-full h-20 md:h-20 aspect-square object-cover' />
+              <img src={image_base_url + cast.profile_path} alt="" className='rounded-full h-20 md:h-20 aspect-square object-cover' />
               <p className='font-medium text-xs mt-3'> {cast.name} </p>
             </div>
           ))}
@@ -79,7 +102,7 @@ const MovieDetails = () => {
 
       <p className='text-lg font-medium mt-20 mb-8'>You May Also Like</p>
       <div className='flex flex-wrap max-sm:justify-center gap-8'>
-          {dummyShowsData.slice(0,4).map((movie, index) => (
+          {shows.slice(0,4).map((movie, index) => (
             <MovieCard key={index} movie={movie}/>
           ))}
       </div>
